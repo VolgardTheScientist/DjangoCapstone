2a0909171250cd6dd73792b489753ec565e94f6b
VolgardTheScientist <piotrpawelpiotrowski@gmail.com>
2024-11-10 19:42:56 +0100

    HTML structure complete

--- djangodelights/inventory/urls.py
+++ djangodelights/inventory/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.home, name="home")
+   path('', views.home, name="home"),
+   path('ingredients/', views.ingredients, name="ingredients"),
+   path('menus/', views.menus, name="menus"),
+   path('purchases/', views.purchases, name="purchases"),
+   path('reports/', views.reports, name="reports"),
 ]
--- djangodelights/inventory/views.py
+++ djangodelights/inventory/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-# Create your views here.
-# def home():
-#   template = loader.get_template("inventory/home.html")
-#   return HttpResponse(template.render())
-
 def home(request):
   return render(request, "inventory/home.html")
+
+def ingredients(request):
+  return render(request, "inventory/ingredients.html")
+
+def menus(request):
+  return render(request, "inventory/menus.html")
+
+def purchases(request):
+  return render(request, "inventory/purchases.html")
+
+def reports(request):
+  return render(request, "inventory/reports.html")
