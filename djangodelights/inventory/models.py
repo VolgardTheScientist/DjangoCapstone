68837d7f935fdbce9340b93bac27b1deab09e2dd
VolgardTheScientist <piotrpawelpiotrowski@gmail.com>
2024-11-11 11:20:31 +0100

    Ingredients Model registered

--- djangodelights/inventory/models.py
+++ djangodelights/inventory/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+    quantity = models.IntegerField(default=0)    
+    unit_price = models.IntegerField(default=0)
+
+    GRAM = "g"
+    KILOGRAM = "kg"
+    LITER = "l"
+    MILLILITER = "ml"
+    TEASPOON = "tsp"
+    TABLESPOON = "tbsp"
+    CUP = "cup"
+    PIECE = "pc"
+    PINCH = "pinch"
+
+    KITCHEN_UNIT_CHOICES = [
+        (GRAM, "Gram"),
+        (KILOGRAM, "Kilogram"),
+        (LITER, "Liter"),
+        (MILLILITER, "Milliliter"),
+        (TEASPOON, "Teaspoon"),
+        (TABLESPOON, "Tablespoon"),
+        (CUP, "Cup"),
+        (PIECE, "Piece"),
+        (PINCH, "Pinch"),
+    ]
+
+    unit = models.CharField(max_length=5, choices=KITCHEN_UNIT_CHOICES, default = GRAM)
+
+    def __str__(self):
+        return self.name 
+    # + ", " + self.quantity + ", " + self.unit+ ", " + self.unit_price
+
+    class Meta:
+        ordering = ["name"]
+
+# class Menu(models.Model):
+#     name = models.CharField(max_length=200)
+#     quantity = models.IntegerField(default=0)
+#     unit = models.IntegerField(default=0)
+#     owner = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+# 
+# class Purchase(models.Model):
+#   pass
+# 
+# class Receipe(models.Model):
+#   pass
