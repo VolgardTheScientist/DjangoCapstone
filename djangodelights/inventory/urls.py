c581d314e6d528abfba0a4dc0b7bc3d0b222c86e
VolgardTheScientist <piotrpawelpiotrowski@gmail.com>
2024-11-12 19:56:57 +0100

    ListViews for MenuItems and Ingredients added

--- djangodelights/inventory/urls.py
+++ djangodelights/inventory/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
    path('', views.home, name="home"),
-   path('ingredients/', views.ingredients, name="ingredients"),
-   path('menuitem/', views.menus, name="menus"),
+   path('ingredients/', views.IngredientsList.as_view(), name='ingredientslist'),
+   path('menuitem/', views.MenusList.as_view(), name="menuslist"),
    path('purchases/', views.purchases, name="purchases"),
    path('reports/', views.reports, name="reports"),
    path('receiperequirements/', views.receiperequirements, name="receiperequirements"),
--- djangodelights/inventory/views.py
+++ djangodelights/inventory/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.views.generic import ListView
+from .models import Ingredient, MenuItem, ReceipeRequirement, Purchase
 
 def home(request):
   return render(request, "inventory/home.html")
 
-def ingredients(request):
-  return render(request, "inventory/ingredients.html")
+# def ingredients(request):
+#   return render(request, "inventory/ingredients.html")
 
-def menus(request):
-  return render(request, "inventory/menuitem.html")
+# def menus(request):
+#   return render(request, "inventory/menuitem.html")
 
 def purchases(request):
   return render(request, "inventory/purchases.html")
@@ -19,3 +19,11 @@ def reports(request):
 
 def receiperequirements(request):
   return render(request, "inventory/receiperequirements.html")
+
+class IngredientsList(ListView):
+  model = Ingredient
+  template_name = "inventory/ingredients.html"
+
+class MenusList(ListView):
+  model = MenuItem
+  template_name = "inventory/menus.html"
